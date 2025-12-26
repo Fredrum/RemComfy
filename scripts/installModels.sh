12a9706fb0f4cbb42703e9083c25243e93da1ea6
Fredrum <fred.sund@gmail.com>
2025-12-25 20:30:57 -0800

    basic setup model load script

---
+++ scripts/installModels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Script to install a series of models into a comfy install"
+
+# use no spaces
+COMFY_PATH="/ComfyUI"
+
+if true; then
+
+	echo "Using comfy location:  $COMFY_PATH"
+fi
